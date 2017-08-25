71b533025a58562f8faf9ee0f69eee29779d9f31
Ayorinde Komolafe <ayorindekomolafe@yahoo.com>
2017-08-25 20:18:14 +0100

    modified config and verify.js

--- config/config.js
+++ config/config.js
@@ -7,5 +7,5 @@ dotenv.load({path: __dirname + '/../env/.env'});
 
 module.exports = Object.freeze({
     TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
-    BASE_URL: process.env.NODE_ENV === 'development' ? process.env.STAGING_URL : process.env.BASE_URL
+    BASE_URL: process.env.LOCAL_ENV ? process.env.LOCAL_URL : process.env.NODE_ENV === 'development' ? process.env.STAGING_URL : process.env.BASE_URL
 });
--- controllers/verify.js
+++ controllers/verify.js
@@ -3,10 +3,69 @@
 const Telegram = require('telegram-node-bot'),
 	axios = require('../config/axios');
 
+function randomChars(length = 8){
+    let data = '',
+    	chars = ['!','#','@','%','^','&','*','_','.'];
+    
+    for(let i = 97; i <= 122; i++){
+        chars.push(String.fromCharCode(i));
+    } 
+    
+    for(let i = 0; i <= length; i++){
+        data += chars[Math.floor(Math.random() * chars.length)];
+    } 
+    return data;
+}
+
 class VerifyController extends Telegram.TelegramBaseController {
     verifyHandler($) {
     	const name = $._message._from._firstName;
+
+    	console.log(randomChars());
+
+    	let user = {},
+    		code = {
+				otp: randomChars(),
+				otpExpires: Date.now + (1000 * 60 * 5)
+			};
+
 		const verify = {
+			email: {
+			    q: 'What\'s your email address please?',
+			    error: 'Account does not exist. Please try again with a valid email address',
+			    validator: (message, callback) => {
+				    if(message.text) {
+						axios.put('/businesses/0?email=' + message.text, code).then(response => {
+							user = response.data.data;
+							console.log(user);
+							return callback(true, message.text);
+						}).catch(err => {
+							console.log(err);
+							callback(false);
+						});
+				    }
+				    callback(false);
+			    }
+
+			},
+			code: {
+			    q: `Your magic code has been sent to the provided email address\n\nPlease type it in here to continue the verification process`,
+			    error: 'Code invalid',
+			    validator: (message, callback) => {
+				    if(message.text) {	
+						axios.get('/businesses/0?code=' + user.otp).then(response => {
+							user = response.data.data;
+							console.log(user);
+							return callback(true, message.text);
+						}).catch(err => {
+							console.log(err);
+							callback(false);
+						});
+				    }
+				    callback(false);
+			    }
+
+			},
 		    company1: {
 			    q: 'Please provide the name of first company?',
 			    error: 'Sorry, wrong input',
@@ -58,6 +117,8 @@ class VerifyController extends Telegram.TelegramBaseController {
 		};
 
 		$.runForm(verify, (result) => {
+			delete result.email;
+			delete result.code;
 			console.log(result);
 				$.sendMessage('Thanks for verifying your business.');
 
